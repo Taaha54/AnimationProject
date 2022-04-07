0953c1faa256fe68112c9aa780b7bc32c1fb8365
Taaha54 <74031473+Taaha54@users.noreply.github.com>
2022-04-06 21:34:33 -0400

    Update Action.java

--- src/Action.java
+++ src/Action.java
@@ -4,6 +4,7 @@
 * @author  Jeffrey Betty
 * @version 1.0
 * @since   2021-04-02 
+* @param   startFrame; type; color[]; jumpX; jumpY; c; a; x; y;
 */
 public class Action {
     private int startFrame;
